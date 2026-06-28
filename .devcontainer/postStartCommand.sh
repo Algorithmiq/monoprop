e0dd942625ec3282ffcece781d09facb5ecc6de6
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2026-06-28 13:08:22 +0200

    chore(deps-dev): 💄 use prek instead of pre-commit

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -8,5 +8,5 @@ echo "Set up development environment in $WORKSPACE"
 # NOTE docs dependency group excluded, as it requires Python 3.12 and we develop against 3.11
 uv sync --group dev --group test --group interactive --all-extras -v
 
-# install pre-commit hooks
-uv run pre-commit install --install-hooks
+# install prek hooks
+uv run prek install --install-hooks
