cbd801d3cf19a1f876c92632b673e8b502a8f5bd
Roberto Di Remigio Eikås <robertodr@users.noreply.github.com>
2026-09-02 10:27:49 +0200

    Apply batched suggestions from code review
    Co-authored-by: Roberto Di Remigio Eikås <robertodr@users.noreply.github.com>
    Signed-off-by: Roberto Di Remigio Eikås <robertodr@users.noreply.github.com>

--- cpp/tests/mpfunctions.cpp
+++ cpp/tests/mpfunctions.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <cstddef>
+
 #include <boost/test/data/monomorphic.hpp>
 #include <boost/test/data/test_case.hpp>
 #include <boost/test/unit_test.hpp>
